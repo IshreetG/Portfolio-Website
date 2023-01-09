553e4842183bd63c2f0574ac466c4567d7882b25
Ishreet Grewal <ishreetgrewal@Ishreets-MacBook-Pro.local>
2023-01-09 18:23:29 -0500

    changes2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,24 @@
 import React from 'react'; 
-import Sidebar from '../sidebar'; 
-import ishreet from '../Images/ishreet1.jpg';
+//import Sidebar from '../sidebar'; 
+//import ishreet from '../Images/ishreet1.jpg';
 import '../App.css';
 
 function Home() {
     return (
-        <div id="App">
-      <div id = "container">
-      <img class="fitting-image" src={ishreet} alt = "Ishreet " />
-      </div>
-      <Sidebar/>
-        <div id= "titlebox">
-        <h1>Hi,</h1>
-        <h2>I'm Ishreet</h2>
-        <h3> Welcome To My Portfolio </h3>
-        </div>
+        //<div id="App">
+        <div>
+          <div class="mainishreet">
+            <div class="caption1">
+              <span class="border1">
+                <h1>Hi,</h1>
+                <h2>I'm Ishreet</h2>
+                <h3> Welcome To My Portfolio </h3>
+
+
+  </span>
+  </div>
+</div>
+
     </div>
 
        
